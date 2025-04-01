4164a40a7881af770271fa941fc375e0462fffcd
Sno <ochoa.sara2036@gmail.com>
2025-04-01 14:49:52 -0700

    vendor.py file from coworking session

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self, item):
+        self.inventory = []
+        self.item = item
+
+    def add_item(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.append(item)
+            return item
+    
+    def remove_item(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
+    
