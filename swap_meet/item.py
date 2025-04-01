9b49b136dd530be9eaf48a8703ef362d7e0c7e2b
Mikaela Baluyot <mikaebal@gmail.com>
2025-04-01 00:50:33 -0700

    implemented item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        # if no given id, generate uuid
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+    def get_category(self):
+        # return class name
+        return self.__class__.__name__
