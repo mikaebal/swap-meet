1de502dad970d271639fb513b71c5951e1aa498e
Mikaela Baluyot <mikaebal@gmail.com>
2025-04-01 00:52:02 -0700

    implemented wave 2 and wave 4 funcs in vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,25 @@
 class Vendor:
-    pass
+    # pass
+
+
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+    # part of wave 2
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+
+    # wave 4
+    def swap_first_item(self, other_vendor):
+        # check if self or other has empty inventory
+            # if yes return False bc can't swap if empty
+        # store both self and other first item in temp variable
+        # remove self and other first item from inventory 
+        # add other first item to self inventory 
+        # add self first item to other inventory 
+        # return True
